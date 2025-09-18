fdbf09d3dfc683b2ede4676a62aea505eb1f4cb8
mame60 <21160@esp.mr>
2025-09-18 00:41:11 +0000

    hii

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://127.0.0.1:8000";
-
+// const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://applesoffres-production.up.railway.app";
 // ---------------- LOGIN ----------------
 export async function loginUser(email, password) {
   const res = await fetch(`${API_BASE_URL}/login`, {
